35f8df12d0f0de445d84d58437658003885bd4d3
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-23 02:05:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,6 @@ def speak_to_grandma(greeting)
   end
 end
 
-speak_to_grandma(greeting)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
