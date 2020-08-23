6a2784f669ad82e08e498e131be05ba75379e86b
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-23 02:03:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(speak)
+def speak_to_grandma(greeting)
   if speak == speak.upcase
     puts "NO, NOT SINCE 1938"
   end
 end
+
+speak_to_grandma(greeting)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
