bacb8c0fe3dcea5f2ccc09a7d59384d185e256a2
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-23 02:22:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else greeting == greeting.
   else greeting == greeting.upcase
     return "HUH?! NOT SINCE 1938!"
   end
